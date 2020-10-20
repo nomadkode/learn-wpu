183afcdbb22aa1482f6bba0be3c9535f06fd6caa
develover22 <62380091+develover22@users.noreply.github.com>
2020-10-21 00:27:26 +0700

    lgtm2

--- js 1/scripts/suwit.js
+++ js 1/scripts/suwit.js
@@ -1,45 +1,40 @@
 let tanya = true;
 while (tanya) {
-  // todo menangkap pilihan player
-  let play = prompt("pilih : gajah, semut, orang");
+    // todo menangkap pilihan player
+    let play = prompt("pilih : gajah, semut, orang");
 
-  // todo menangkap pilihan komputer
-  // * menggenerate  bilangan random
-  let com = Math.random();
+    // todo menangkap pilihan komputer
+    // * menggenerate  bilangan random
+    let com = Math.random();
 
-  if (com < 0.34) {
-    com = "gajah";
-  } else if (com >= 0.34 && com < 0.67) {
-    com = "orang";
-  } else {
-    com = "semut";
-  }
+    if (com < 0.34) {
+        com = "gajah";
+    } else if (com >= 0.34 && com < 0.67) {
+        com = "orang";
+    } else {
+        com = "semut";
+    }
 
-  let hasil = "";
-  // todo menentukan rules
-  if (play == com) {
-    hasil = "SERI!";
-  } else if (play == "gajah") {
-    // if (comp == "orang") {
-    //     hasil = "MENANG!";
-    // } else {
-    //     hasil = "KALAH";
-    // }
-    hasil = com == "orang" ? "MENANG!" : "KALAH!";
-  } else if (play == "orang") {
-    hasil = com == "gajah" ? "KALAH!" : "MENANG!";
-  } else if (play == "semut") {
-    hasil = com == "orang" ? "KALAH" : "MENANG";
-  } else {
-    hasil = "memasukkan pilihan yang salah!";
-  }
+    // todo menentukan rules
+    let hasil = "";
+    if (play == com) {
+        hasil = "SERI!";
+    } else if (play == "gajah") {
+        hasil = com == "orang" ? "MENANG!" : "KALAH!";
+    } else if (play == "orang") {
+        hasil = com == "gajah" ? "KALAH!" : "MENANG!";
+    } else if (play == "semut") {
+        hasil = com == "orang" ? "KALAH" : "MENANG";
+    } else {
+        hasil = "memasukkan pilihan yang salah!";
+    }
 
-  // todo tampilan hasilnya
-  alert(
-    `kamu memilih : ${play} dan komputer memilih : ${com} \n maka hasilnya : kamu ${hasil}`
-  );
+    // todo tampilan hasilnya
+    alert(
+        `kamu memilih : ${play} dan komputer memilih : ${com} \n maka hasilnya : kamu ${hasil}`
+    );
 
-  tanya = confirm("Lagi?");
+    tanya = confirm("Lagi?");
 }
 
 alert("terima kasih sudah bermain!");
